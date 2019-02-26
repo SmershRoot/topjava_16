0a6f37d12ea794191f96d10fddcc73562ca686ca
Scherbakov Alexandr <Smersh.Root@gmail.com>
2019-02-26 18:03:56 +0500

    HW3. Основное задание

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -24,7 +25,7 @@ CREATE TABLE user_roles
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE TABLE MEALS (
+CREATE TABLE meals (
   id INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
   user_id INTEGER NOT NULL,
   date_time TIMESTAMP,
